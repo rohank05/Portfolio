58e7637f3aae4c5edee7b3f237b349ea83d11e08
Rohan Kumar <rohan.shuvam@gmail.com>
2024-02-04 13:03:05 +0530

    added sound and links

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,23 @@ import {
 import Header from "./components/Header";
 import Social from "./components/Social";
 import Profile from "./components/Profile";
+import useSound from "use-sound";
+import lightswitchAudio from "./assets/lightswitch.mp3";
 
 function App() {
     const { colorMode, toggleColorMode } = useColorMode();
     const isDark = colorMode === "dark";
+    const [play] = useSound(lightswitchAudio, {
+        volume: 0.05,
+        sprite: {
+            on: [0, 300],
+            off: [500, 300],
+        },
+    });
+    const handleColorSwitch = () => {
+        isDark ? play({ id: "on" }) : play({ id: "off" });
+        toggleColorMode();
+    };
     return (
         <VStack p={5}>
             <Flex w={"100%"}>
@@ -35,25 +48,37 @@ function App() {
                 <IconButton
                     icon={<FaLinkedin />}
                     isRound="true"
-                    onClick={toggleColorMode}
+                    onClick={() =>
+                        window.open(
+                            "https://www.linkedin.com/in/rohank05/",
+                            "_blank"
+                        )
+                    }
                 ></IconButton>
                 <IconButton
                     ml={2}
                     icon={<FaInstagram />}
                     isRound="true"
-                    onClick={toggleColorMode}
+                    onClick={() =>
+                        window.open(
+                            "https://www.instagram.com/rohank1251/",
+                            "_blank"
+                        )
+                    }
                 ></IconButton>
                 <IconButton
                     ml={2}
                     icon={<FaGithub />}
                     isRound="true"
-                    onClick={toggleColorMode}
+                    onClick={() =>
+                        window.open("https://github.com/rohank05", "_blank")
+                    }
                 ></IconButton>
                 <IconButton
                     ml={8}
                     icon={isDark ? <FaSun /> : <FaMoon />}
                     isRound="true"
-                    onClick={toggleColorMode}
+                    onClick={handleColorSwitch}
                 ></IconButton>
             </Flex>
             <Header></Header>
